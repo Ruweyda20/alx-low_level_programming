4e7ed3198dd9ff59e7c67922b266282e18d908d5
Ruweyda20 <ruweidaelmi13@gmail.com>
2022-12-14 01:50:47 -0800

    Multiple mains

--- 0x03-debugging/0-main.c
+++ 0x03-debugging/0-main.c
@@ -7,19 +7,10 @@
 
 int main(void)
 {
-<<<<<<< HEAD
 	int i;
 
 	i = 0;
 	positive_or_negative(i);
 
 	return (0);
-=======
-         int i;
-
-         i = 0;
-	positive_or_negative(i);
-
-         return (0);
->>>>>>> 7a9e09f8f7c1fe2c5903baf99c8caffc71def4cd
 }
--- 0x03-debugging/main.h
+++ 0x03-debugging/main.h
@@ -3,7 +3,6 @@
 
 #include <stdio.h>
 
-<<<<<<< HEAD
 void positive_or_negative(int i);
 int largest_number(int a, int b, int c);
 int convert_day(int month, int day);
@@ -11,12 +10,3 @@ void print_remaining_days(int month, int day, int year);
 void print_times_tables(int n);
 
 #endif /* MAIN_H */
-=======
- void positive_or_negative(int i);
- int largest_number(int a, int b, int c);
- int convert_day(int month, int day);
- void print_remaining_days(int month, int day, int year);
- void print_times_tables(int n);
-
- #endif /* MAIN_H */
->>>>>>> 7a9e09f8f7c1fe2c5903baf99c8caffc71def4cd
