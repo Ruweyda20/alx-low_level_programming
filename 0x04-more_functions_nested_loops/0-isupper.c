9e4e61314c9e70f40219bdbe77eaeef5d0474cd2
Ruweyda20 <ruweidaelmi13@gmail.com>
2022-12-15 03:55:22 -0800

    0. isupper

--- 0x04-more_functions_nested_loops/0-isupper.c
+++ 0x04-more_functions_nested_loops/0-isupper.c
@@ -1,11 +1,8 @@
 #include "main.h"
 
 /**
-<<<<<<< HEAD
  *  _isupper - checks uppercase
-=======
  * _isupper - checks uppercase
->>>>>>> efeef7594c0f0c83b0056b5880b2042a9bbf6b86
  *  @c: parameter
  *  Return: 0 if successful
  */
